38badd5703054c0045644457ff9322f460ff6307
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-06-22 16:24:09 +0530

    Endpoint changed

--- app/Utils/index.js
+++ app/Utils/index.js
@@ -1,4 +1,4 @@
 // const URL = "http://localhost:5000/api";
-const URL = "https://rajsareesserver.onrender.com/";
+const URL = "https://rajsareesserver.onrender.com/api";
 // const URL = "https://rajsareesenterprises.com:5000/api";
 export default URL;
